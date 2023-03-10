092376d654fd097c734984f492528d4ba2e99b0a
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-03-11 00:07:16 +0300

    fix:images optimisation

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ const page = () => {
           <h2 className="card-title">Ecommerce Dashboard</h2>
           <p>A responsive e-commerce and finance dashboard</p>
           <div className="card-actions justify-end">
-            <Link href={"https://res.cloudinary.com/dhciks96e/image/upload/v1678480548/dashboard_cwkx7m.png"}>
+            <Link href="https://res.cloudinary.com/dhciks96e/image/upload/v1678480548/dashboard_cwkx7m.png">
               {" "}
               <button className="btn btn-primary">Visit Site</button>
             </Link>
@@ -28,7 +28,7 @@ const page = () => {
           <h2 className="card-title">Techivity</h2>
           <p>Techivity is a project management software. It enables teams to track tasks and bugs on a visual board with drag and drop functionality. </p>
           <div className="card-actions justify-end">
-          <Link href={"https://res.cloudinary.com/dhciks96e/image/upload/v1678480552/task_whbfpx.png"}>
+          <Link href="https://res.cloudinary.com/dhciks96e/image/upload/v1678480552/task_whbfpx.png">
               {" "}
               <button className="btn btn-primary">Visit Site</button>
             </Link>
@@ -43,7 +43,7 @@ const page = () => {
           <h2 className="card-title">Jenga</h2>
           <p>Jenga is a search and comparison website dedicated to home furniture and furnishings. It provides access to over 110 furniture retailers and their complete product inventory in a single location, making it easy for consumers to find what they are looking for.</p>
           <div className="card-actions justify-end">
-          <Link href={"https://res.cloudinary.com/dhciks96e/image/upload/v1678480549/AI_ddqszo.png"}>
+          <Link href="https://res.cloudinary.com/dhciks96e/image/upload/v1678480549/AI_ddqszo.png">
               {" "}
               <button className="btn btn-primary">Visit Site</button>
             </Link>
