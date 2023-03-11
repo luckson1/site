d04936e72b8ab4bb4d2db15b92b261aa12604f74
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-03-11 23:42:29 +0300

    fix: open new tap for site links

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,13 +7,17 @@ const page = () => {
     <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
       <div className="card w-full glass shadow-accent shadow-lg ">
         <figure className="relative w-full h-[300px]">
-          <Image src="https://res.cloudinary.com/dhciks96e/image/upload/v1678480548/dashboard_cwkx7m.png" fill alt="web-screenshot" />
+          <Image
+            src="https://res.cloudinary.com/dhciks96e/image/upload/v1678480548/dashboard_cwkx7m.png"
+            fill
+            alt="web-screenshot"
+          />
         </figure>
         <div className="card-body">
           <h2 className="card-title">Ecommerce Dashboard</h2>
           <p>A responsive e-commerce and finance dashboard</p>
           <div className="card-actions justify-end">
-            <Link href="https://dashboarding.netlify.app/">
+            <Link href="https://dashboarding.netlify.app/" target="_blank">
               {" "}
               <button className="btn btn-primary">Visit Site</button>
             </Link>
@@ -22,13 +26,21 @@ const page = () => {
       </div>
       <div className="card w-full glass shadow-accent shadow-lg ">
         <figure className="relative w-full h-[300px]">
-          <Image src="https://res.cloudinary.com/dhciks96e/image/upload/v1678480552/task_whbfpx.png" fill alt="web-screenshot" />
+          <Image
+            src="https://res.cloudinary.com/dhciks96e/image/upload/v1678480552/task_whbfpx.png"
+            fill
+            alt="web-screenshot"
+          />
         </figure>
         <div className="card-body">
           <h2 className="card-title">Techivity</h2>
-          <p>Techivity is a project management software. It enables teams to track tasks and bugs on a visual board with drag and drop functionality. </p>
+          <p>
+            Techivity is a project management software. It enables teams to
+            track tasks and bugs on a visual board with drag and drop
+            functionality.{" "}
+          </p>
           <div className="card-actions justify-end">
-          <Link href="https://techivity.netlify.app">
+            <Link href="https://techivity.netlify.app" target="_blank">
               {" "}
               <button className="btn btn-primary">Visit Site</button>
             </Link>
@@ -37,13 +49,22 @@ const page = () => {
       </div>
       <div className="card w-full glass shadow-accent shadow-lg ">
         <figure className="relative w-full h-[300px]">
-          <Image src="https://res.cloudinary.com/dhciks96e/image/upload/v1678480549/marketplace_vlbynq.png" fill alt="web-screenshot" />
+          <Image
+            src="https://res.cloudinary.com/dhciks96e/image/upload/v1678480549/marketplace_vlbynq.png"
+            fill
+            alt="web-screenshot"
+          />
         </figure>
         <div className="card-body">
           <h2 className="card-title">Jenga</h2>
-          <p>Jenga is a search and comparison website dedicated to home furniture and furnishings. It provides access to over 110 furniture retailers and their complete product inventory in a single location, making it easy for consumers to find what they are looking for.</p>
+          <p>
+            Jenga is a search and comparison website dedicated to home furniture
+            and furnishings. It provides access to over 110 furniture retailers
+            and their complete product inventory in a single location, making it
+            easy for consumers to find what they are looking for.
+          </p>
           <div className="card-actions justify-end">
-          <Link href="https://jenga.vercel.app/">
+            <Link href="https://jenga.vercel.app/" target="_blank">
               {" "}
               <button className="btn btn-primary">Visit Site</button>
             </Link>
@@ -52,13 +73,21 @@ const page = () => {
       </div>
       <div className="card w-full glass shadow-accent shadow-lg ">
         <figure className="relative w-full h-[300px]">
-          <Image src="https://res.cloudinary.com/dhciks96e/image/upload/v1678480549/AI_ddqszo.png" fill alt="web-screenshot" />
+          <Image
+            src="https://res.cloudinary.com/dhciks96e/image/upload/v1678480549/AI_ddqszo.png"
+            fill
+            alt="web-screenshot"
+          />
         </figure>
         <div className="card-body">
           <h2 className="card-title">Sasha</h2>
-          <p>Sasha is student writing assistant. It uses AI to help in idea generation as well as speed the research process, saving students time taken to complete their assignments</p>
+          <p>
+            Sasha is student writing assistant. It uses AI to help in idea
+            generation as well as speed the research process, saving students
+            time taken to complete their assignments
+          </p>
           <div className="card-actions justify-end">
-          <Link href={"https://www.smartsasha.com/"}>
+            <Link href={"https://www.smartsasha.com/"} target="_blank">
               {" "}
               <button className="btn btn-primary">Visit Site</button>
             </Link>
