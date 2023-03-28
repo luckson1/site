da3e039c54645c2a3d944a347066efdc255d83b0
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-03-28 22:49:00 +0300

    fix: better nav buttons

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -12,16 +12,16 @@ const NavBar = () => {
   return (
 
 <nav className=" navbar bg-base-100 mb-32">
-<Link href={"/"} className={`btn normal-case text-xs md:text-xl mr-2 md:mr-4 ${route==="/"? "": "btn-ghost"}`}>
+<Link href={"/"} className={`btn btn-sm  w-16 md:w-28 normal-case text-xs md:text-base mr-2 md:mr-4 ${route==="/"? "": "btn-ghost"}`}>
 About
 </Link>
-<Link href={"/resume"} className={`btn normal-case text-xs md:text-xl mr-2 md:mr-4 ${route==="/resume"? "": "btn-ghost"}`}>
+<Link href={"/resume"} className={`btn  btn-sm  w-16 md:w-28 normal-case text-xs md:text-base mr-2 md:mr-4 ${route==="/resume"? "": "btn-ghost"}`}>
 Resume
 </Link>
-<Link href={"/projects"} className={`btn normal-case text-xs md:text-xl mr-2 md:mr-4 ${route==="/projects"? "": "btn-ghost"}`}>
+<Link href={"/projects"} className={`btn  btn-sm  w-16 md:w-28 normal-case text-xs md:text-base mr-2 md:mr-4 ${route==="/projects"? "": "btn-ghost"}`}>
 Projects
 </Link>
-<Link href={"/blog"} className={`btn normal-case text-xs md:text-xl mr-2 md:mr-4 ${route==="/blog"? "": "btn-ghost"}`}>
+<Link href={"/blog"} className={`btn  btn-sm  w-16 md:w-28 normal-case text-xs md:text-base mr-2 md:mr-4 ${route==="/blog"? "": "btn-ghost"}`}>
 Blog
 </Link>
 
