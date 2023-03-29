162d03553c321d0a76418b3c9f553a5b0dd80afc
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-03-29 17:26:37 +0300

    fix:nav animation

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -11,7 +11,7 @@ const NavBar = () => {
   const route=usePathname()
   return (
 
-<nav className=" navbar bg-base-100 mb-32">
+<nav className=" navbar bg-base-100 mb-32 animate-fade-in">
 <Link href={"/"} className={`btn btn-sm  w-16 md:w-28 normal-case text-xs md:text-base mr-2 md:mr-4 ${route==="/"? "": "btn-ghost"}`}>
 About
 </Link>
