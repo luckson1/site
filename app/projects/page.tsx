e452e44b7d0353f5cadd039ccb2356904f05f32e
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-05-15 01:07:08 +0300

    feat: add navigation to repository

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-
+import {AiFillGithub} from "react-icons/ai"
 const page = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
@@ -16,10 +16,14 @@ const page = () => {
         <div className="card-body">
           <h2 className="card-title">Ecommerce Dashboard</h2>
           <p>A responsive e-commerce and finance dashboard</p>
-          <div className="card-actions justify-end">
+          <div className="card-actions justify-between">
             <Link href="https://dashboarding.netlify.app/" target="_blank">
               {" "}
               <button className="btn btn-primary">Visit Site</button>
+            </Link>
+            <Link href="https://github.com/luckson1/dashboard" target="_blank">
+              {" "}
+              <button className="btn btn-secondary gap-2 btn-outline"> <AiFillGithub className="w-6 h-6"/> Repository</button>
             </Link>
           </div>
         </div>
@@ -39,10 +43,14 @@ const page = () => {
             track tasks and bugs on a visual board with drag and drop
             functionality.{" "}
           </p>
-          <div className="card-actions justify-end">
+          <div className="card-actions justify-between">
             <Link href="https://techivity.netlify.app" target="_blank">
               {" "}
               <button className="btn btn-primary">Visit Site</button>
+            </Link>
+            <Link href="https://github.com/luckson1/Productivity" target="_blank">
+              {" "}
+              <button className="btn btn-secondary gap-2 btn-outline"> <AiFillGithub className="w-6 h-6"/> Repository</button>
             </Link>
           </div>
         </div>
@@ -50,7 +58,7 @@ const page = () => {
       <div className="card w-full glass  ">
         <figure className="relative w-full h-[200px] md:h-[300px]">
           <Image
-            src="https://res.cloudinary.com/dhciks96e/image/upload/v1678480549/marketplace_vlbynq.png"
+            src="https://res.cloudinary.com/dhciks96e/image/upload/v1684097970/Screen_Shot_2023-05-14_at_11.58.01_PM_art6p5.png"
             fill
             alt="web-screenshot"
           />
@@ -63,33 +71,40 @@ const page = () => {
             and their complete product inventory in a single location, making it
             easy for consumers to find what they are looking for.
           </p>
-          <div className="card-actions justify-end">
-            <Link href="https://jenga.vercel.app/" target="_blank">
+          <div className="card-actions justify-between">
+          <Link href="https://jenga.vercel.app/" target="_blank">
               {" "}
               <button className="btn btn-primary">Visit Site</button>
             </Link>
+          <Link href="https://github.com/luckson1/Jenga" target="_blank">
+              {" "}
+              <button className="btn btn-secondary gap-2 btn-outline"> <AiFillGithub className="w-6 h-6"/> Repository</button>
+            </Link>
+          
           </div>
         </div>
       </div>
       <div className="card w-full glass  ">
         <figure className="relative w-full h-[200px] md:h-[300px]">
           <Image
-            src="https://res.cloudinary.com/dhciks96e/image/upload/v1678480549/AI_ddqszo.png"
+            src="https://res.cloudinary.com/dhciks96e/image/upload/v1684099439/Screen_Shot_2023-05-15_at_12.23.40_AM_nq8zlm.png"
             fill
             alt="web-screenshot"
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Sasha</h2>
+          <h2 className="card-title">PawfectAfrica</h2>
           <p>
-            Sasha is student writing assistant. It uses AI to help in idea
-            generation as well as speed the research process, saving students
-            time taken to complete their assignments
+          This app can be used by pet rescue NGOs to manage the adoption process. It features a Tinder-style dashboard with swipeable cards that make adoption easy and fun.
           </p>
-          <div className="card-actions justify-end">
-            <Link href={"https://www.smartsasha.com/"} target="_blank">
+          <div className="card-actions justify-between">
+            <Link href={"https://www.pawfectafrica.com/"} target="_blank">
               {" "}
               <button className="btn btn-primary">Visit Site</button>
+            </Link>
+            <Link href="https://github.com/luckson1/pawfectafrica" target="_blank">
+              {" "}
+              <button className="btn btn-secondary gap-2 btn-outline"> <AiFillGithub className="w-6 h-6"/> Repository</button>
             </Link>
           </div>
         </div>
